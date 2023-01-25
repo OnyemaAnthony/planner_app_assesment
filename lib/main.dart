9fc1878d130b230aec24020669d914e82bcff73e
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-01-26 01:37:21 +0800

    clean up comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:planner_app/screens/add_task_screen.dart';
 import 'package:planner_app/screens/home_screen.dart';
-import 'package:planner_app/services/cache/cache.dart';
 import 'package:planner_app/services/cache/cache_impl.dart';
-import 'package:planner_app/usecase/task/add_task_case.dart';
-import 'package:planner_app/usecase/user/login_user_case.dart';
-import 'package:planner_app/utils/setup.dart';
-
 import 'di/injection.dart';
-import 'models/user/user_request.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
