14fe87e8c5d7d83f7e0aeac1107189d372a396cd
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-02-12 19:42:21 +0100

    remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:planner_app/screens/home_screen.dart';
-import 'package:planner_app/services/cache/cache_impl.dart';
-import 'di/injection.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
