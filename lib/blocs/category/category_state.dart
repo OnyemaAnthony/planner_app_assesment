246cb9edf767908d48e543fcb4760e0abecfd033
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-05-19 21:48:06 +0100

    remove props

--- lib/blocs/category/category_state.dart
+++ lib/blocs/category/category_state.dart
@@ -1,32 +1,29 @@
 part of 'category_bloc.dart';
 
 abstract class CategoryState extends Equatable {
+  @override
+  List<Object> get props => [];
   const CategoryState();
 }
 
 class CategoryInitial extends CategoryState {
-  @override
-  List<Object> get props => [];
+
 }
 class CategoriesLoadedState extends CategoryState{
   final List<Documents> categories;
 
   const CategoriesLoadedState(this.categories);
-  @override
-  List<Object?> get props => [];
+
 
 }
 class CategoryErrorState extends CategoryState{
   final String errorMessage;
 
   const CategoryErrorState(this.errorMessage);
-  @override
-  List<Object?> get props => [];
+
 
 }
 
 class CategoryLoadingState extends CategoryState{
-  @override
-  List<Object?> get props => [];
 
 }
